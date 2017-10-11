580f3079a77baffd28b540c5189760ac8e8875ae
Mikhail Pak <mikhail.pak@tum.de>
2017-10-11 14:36:38 +0200

    Explain a magic number

--- laser-eyes.ino
+++ laser-eyes.ino
@@ -2,6 +2,7 @@
 
 const uint16_t NUM_LEDS = 2;
 const uint8_t PIN = 6;
+const float PI_BY_255 = 0.01231997119;
 
 Adafruit_NeoPixel eyes = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRB + NEO_KHZ800);
 
@@ -21,7 +22,7 @@ void breathe_blue(uint8_t wait) {
   float m;
 
   for (j = 0; j < 256; j++) {
-    m = sin(j * 0.01231997119);
+    m = sin(j * PI_BY_255);
     eyes.setPixelColor(0, 0, int(101.0 * m), int(189.0 * m)); // TUM blue
     eyes.setPixelColor(1, 0, int(101.0 * m), int(189.0 * m)); // TUM blue
     eyes.show();
@@ -34,7 +35,7 @@ void breathe_orange(uint8_t wait) {
   float m;
 
   for (j = 0; j < 256; j++) {
-    m = sin(j * 0.01231997119);
+    m = sin(j * PI_BY_255);
     eyes.setPixelColor(0, int(227.0 * m), int(114.0 * m), int(34.0 * m)); // TUM orange
     eyes.setPixelColor(1, int(227.0 * m), int(114.0 * m), int(34.0 * m)); // TUM orange
     eyes.show();
